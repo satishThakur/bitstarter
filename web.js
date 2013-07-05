882466c1fdde9069b7d2d3076bc7ac48a54a3b15
Satish <satish@Satishs-MacBook-Air.local>
2013-07-06 01:19:15 +0530

    added index

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs  = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var content = fs.readFileSync('./index.html');
+    console.log('Content Type: ' + typeof content);
+    response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
